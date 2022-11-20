7243940895871c5748842c856767b3c0c77c572a
dmitriipolushin <27102001@bk.ru>
2022-11-20 13:01:27 +0300

    fix

--- app_python/base_tests.py
+++ app_python/base_tests.py
@@ -5,4 +5,5 @@ import pytz
 
 def test_dateformat():
     time_now = datetime.now(pytz.timezone('Europe/Moscow'))
-    assert Time_controller().moscow_time() == time_now.strftime("%d/%m/%Y %H:%M:%S")
+    time = time_now.strftime("%d/%m/%Y %H:%M:%S")
+    assert Time_controller().moscow_time() == time
